95061ea92ddf9f0ef14200fdcf97423149da2743
Orthoceras <florian.berthier@hotmail.com>
2024-05-28 12:45:25 +0200

    feat: restaurantController create

--- back/src/Controller/RestaurantController.php
+++ back/src/Controller/RestaurantController.php
@@ -20,7 +20,7 @@ class RestaurantController extends AbstractController
         ]);
     }
 
-    #[Route('/api/restaurants', name: 'restaurant.getAll')]
+    #[Route('/api/restaurants', name: 'restaurant.getAll', methods: ["GET"])]
     public function getAllRestaurants(RestaurantRepository $repository, SerializerInterface $serializer): JsonResponse
     {
         $restaurants = $repository->findAll();
@@ -28,11 +28,19 @@ class RestaurantController extends AbstractController
         return new JsonResponse($jsonRestaurants, JsonResponse::HTTP_OK, [], true);
     }
 
-    #[Route('/api/restaurant/{idRestaurant}', name: 'restaurant.get')]
+    #[Route('/api/restaurants/{idRestaurant}', name: 'restaurant.get', methods: ["GET"])]
     public function getRestaurant(int $idRestaurant, RestaurantRepository $repository, SerializerInterface $serializer): JsonResponse
     {
-        $restaurants = $repository->find($idRestaurant);
-        $jsonRestaurants = $serializer->serialize($restaurants, 'json');
-        return new JsonResponse($jsonRestaurants, JsonResponse::HTTP_OK, [], true);
+        $restaurant = $repository->find($idRestaurant);
+        $jsonRestaurant = $serializer->serialize($restaurant, 'json');
+        return new JsonResponse($jsonRestaurant, JsonResponse::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/restaurants/', name: 'restaurant.create', methods: ["POST"])]
+    public function createRestaurant(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager ){
+        $restaurant = $serializer->deserialize($request->getContent(), Song::class, 'json');
+        
+        $entityManager->persist($restaurant);
+        $entityManager->flush();
     }
 }
